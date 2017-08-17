32e3dbce8e09244d58c4ee755279794384b40d9f
luojiangyu <luojiangyu@didichuxing.com>
2017-08-17 21:04:57 +0800

    modify

--- src/main/scala/com/didichuixing/flink/Launcher.scala
+++ src/main/scala/com/didichuixing/flink/Launcher.scala
@@ -1,12 +1,14 @@
 package com.didichuxing.flink
 
-import java.text.SimpleDateFormat
-import java.util.Date
+import java.util.{Date, Properties}
 
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema
+
 
 /**
   * Created by luojiangyu on 2/28/17.
@@ -15,14 +17,20 @@ object Launcher {
 
   def main(args: Array[String]) {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
-    env.enableCheckpointing(5000)
-    val stream = env.addSource(KafkaSourceConfig.getKafkaSource(args(0)))
+    /* Checkpoint */
+    env.enableCheckpointing(60000L)
+
+    val consumerProperties = new Properties
+    consumerProperties.put("bootstrap.servers", args(0))
+    consumerProperties.put("group.id", args(1))
+
+    val stream = env.addSource(new FlinkKafkaConsumer010[String](args(2), new SimpleStringSchema, consumerProperties))
     stream.flatMap(_.toLowerCase().split("\\W+"))
-      .map(a => (a + "-" + new SimpleDateFormat("yyyy-MM-dd(HH:mm)").format(new Date(System.currentTimeMillis()))
-        , 1))
+      .map(a => (a,1))
       .keyBy(0)
       .window(TumblingProcessingTimeWindows.of(Time.seconds(60)))
-      .sum(1).print()
+      .sum(1)
+      .print()
 
     env.execute()
 
